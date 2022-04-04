45b7b455b3fb8fce9d3fa62d0de26378c39d2d99
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-04-04 13:58:27 +0600

    evening commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {  useState } from 'react';
+
 
 import Watch from "../../images/post_1_3312.jpg"
 
+import ratingsData from "../../fakeData/fakeData" 
+
 import './Home.css'
+import Review from '../Review/Review';
 
 const Home = () => {
+
+    const [watches, setWatches] = useState(ratingsData);
+
+   
+  
+    
     return (
         <div>
             <section className='homepart'>
@@ -34,12 +44,49 @@ const Home = () => {
 
 
             </section>
+
+
+            {/* CustomerReview (3) */}
+
+
+            <section className='section bg-light border-top'>
+
+                <div className='container'>
+                    <div className="row">
+                        
+                            <div className="col-md-12 ">
+                            <h3 className='text-center'> Our Customer's Review(3)</h3>
+                            <div className="underline mx-auto"></div>
+
+                            <div className='homecard '>
+                 {
+                              
+                                watches.slice(0,3).map((watch) => (
+                                    
+                                    <p>Name :{watch.name}
+                                    
+                                        <p>   Id:{watch.id}</p>
+                                        <p>  Description :{watch.description}</p>
+                                        
+                                        <p>Ratings:{ watch.ratings }</p>
+                                  
+                                    </p>
+                                    
+                                 
+                                 
+                                 ))
+} 
             
-            <section>
+
+                            </div>
+                        
+                        </div>
+                        
+                        <button className='btb btb-lg'>See All Reviews</button>
+                  </div>
+</div>
 
 
-
-                
             </section>
     
         </div>
