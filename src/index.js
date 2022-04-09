41ab2eea2cd356fab59a687560363c6d93e044f4
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-04-09 08:14:50 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOMClient from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import ReactDOMClient from 'react-dom/client';
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
 root.render(
